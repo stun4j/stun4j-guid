58382e48ee15fba2c8db15ebb152748cc329d72b
jmeng <spirit.jay@163.com>
2020-02-23 23:57:53 +0800

    * perf:bench performance tune

--- src/test/java/com/stun4j/guid/LocalGuidBenchmark.java
+++ src/test/java/com/stun4j/guid/LocalGuidBenchmark.java
@@ -1,7 +1,8 @@
 package com.stun4j.guid;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import com.codahale.metrics.ConsoleReporter;
@@ -10,14 +11,17 @@ import com.codahale.metrics.MetricRegistry;
 import com.stun4j.guid.utils.Utils;
 
 public class LocalGuidBenchmark {
-  static final ExecutorService E = Executors.newWorkStealingPool();
+  // static final ExecutorService E = Executors.newWorkStealingPool();
+  static final ExecutorService E = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(),
+      Runtime.getRuntime().availableProcessors(), 0L, TimeUnit.MILLISECONDS,
+      new LinkedBlockingQueue<Runnable>(1000), new ThreadPoolExecutor.CallerRunsPolicy());
 
   public static void main(String[] args) {
     LocalGuid guid = LocalGuid.init(0, 0);
     // warm round
-//    for (int i = 0; i < 10_0000; i++) {
-//      guid.next();
-//    }
+    for (int i = 0; i < 10_0000; i++) {
+      guid.next();
+    }
     // benchmark round
     MetricRegistry registry = new MetricRegistry();
     ConsoleReporter reporter = ConsoleReporter.forRegistry(registry).build();
@@ -25,13 +29,13 @@ public class LocalGuidBenchmark {
     long start = System.currentTimeMillis();
     reporter.start(0, 250, TimeUnit.MILLISECONDS);
     // start
-    for (int i = 0; i < 1000_0000; i++) {
+    for (int i = 0; i < 5000_0000; i++) {
       E.execute(() -> {
         guid.next();
       });
       meter.mark();
     }
     System.out.println(System.currentTimeMillis() - start);
-    Utils.sleepSeconds(30);
+    Utils.sleepSeconds(10);
   }
 }
